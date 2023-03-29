8d8ffe01c22bfadbaf261d55afb726c65e94ef08
HOUSSAM0F <atiche.hossam7@gmail.com>
2023-03-28 17:56:06 -0700

    C More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two intergers
+ * @c: The first number to multiply
+ * @d: The second number to multiply with first number
+ * Return: The result of multiplication between c param and d param
+ */
+int mul(int c, int d)
+{
+return (c * d);
+}
