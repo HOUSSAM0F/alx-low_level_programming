0685607c287d6283c02c6ff1daa6b83f77b008d4
HOUSSAM0F <atiche.hossam7@gmail.com>
2023-04-16 01:33:07 +0000

    ad everything

--- 0x09-static_libraries/my_static_library/create_static_lib.sh
+++ 0x09-static_libraries/my_static_library/create_static_lib.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-
-# compile each .c file into an object file
-for file in *.c; do
-gcc -c "$file" -o "${file%.c}.o"
-done
-
-# Create the static library
-ar rcs liball.a *.o
-
-# Remove the object files
-rm *.o
+gcc -Wall -Wextra -Werror -pedantic -c *.c
+ar rc liball.a *.o
