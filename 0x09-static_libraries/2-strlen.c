f4286d5eb6e3bf39a0194d40d8b85e903c8bc39e
HOUSSAM0F <atiche.hossam7@gmail.com>
2023-04-16 02:05:47 +0000

    EVERYTHING IS OKAY

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,18 +1,14 @@
-#include "main.h"
+#include <stdio.h>
+
 /**
-* _strlen - returns the length of a string
-* @s: string
-* Return: length
-*/
-int _strlen(char *s)
+ * _strlen - Returns the length of a string
+ * @str: The string to get the length of
+ * Return: The length of @str
+ */
+size_t _strlen(const char *str)
 {
-int longi = 0;
-
-while (*s != '\0')
-{
-longi++;
-s++;
-}
-
-return (longi);
+size_t length = 0;
+while (*str++)
+length++;
+return (length);
 }
