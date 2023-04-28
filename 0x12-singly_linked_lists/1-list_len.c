a5314fea333b272553f7be665b3e9f2dc0a98a3d
HOUSSAM0F <atiche.hossam7@gmail.com>
2023-04-28 02:22:12 +0100

    second  commit

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,12 +9,13 @@
 */
 size_t list_len(const list_t *h)
 {
-size_t n = 0;
+size_t n = 0; // initialize n to 0
+const list_t *current = h; // use a pointer to traverse the list
 
-while (h)
+while (current != NULL) // loop until the end of the list
 {
-n++;
-h = h->next;
+n++; // increment n by 1
+current = current->next; // move to the next node
 }
-return (n);
+return (n); // return the number of elements
 }
