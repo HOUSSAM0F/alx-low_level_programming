2c061ee63aa39db79c4ad1f32337bd65eb5ba176
HOUSSAM0F <atiche.hossam7@gmail.com>
2023-05-02 15:55:35 +0100

    0. Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,50 +1,18 @@
-#include <stdio.h>
-
+#include "lists.h"
 /**
-* struct listint_s - singly linked list
-* @n: integer
-* @next: points to the next node
-*
-* Description: singly linked list node structure
-* 
-*/
-typedef struct listint_s
-{
-int n;
-struct listint_s *next;
-} listint_t;
-
-/**
-* print_listint - prints all the elements of a listint_t list
-* @h: pointer to the head of the list
-*
-* Return: the number of nodes in the list
+*print_listint - function that prints all the elements of a listint_t list
+*@h: Pointer to the head node
+*Return: The number of nodes
 */
 size_t print_listint(const listint_t *h)
 {
-size_t count = 0;
-while (h != NULL) {
+int num = 0;
+
+while (h)
+{
 printf("%d\n", h->n);
 h = h->next;
-count++;
+num++;
 }
-return count;
-}
-
-int main(void)
-{
-listint_t *head = NULL;
-
-/* Add some nodes to the list */
-head = add_node(head, 1);
-head = add_node(head, 2);
-head = add_node(head, 3);
-
-/* Print the list */
-size_t count = print_listint(head);
-
-/* Print the number of nodes */
-printf("The list has %zu nodes\n", count);
-
-return 0;
+return (num);
 }
